7541529026abad796cb99b8481aa723950728e66
davidbendumbari <155819810+davidbendumbari@users.noreply.github.com>
2026-06-08 19:49:16 +0000

    finally uncommented the file

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -58,13 +58,3 @@ def test_get_nth_fibonacci_ten():
     assert result == 89
 
 
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
-
-#     # Act
-#     result = get_nth_fibonacci(n)
-
-#     # Assert
-#     assert result == 89
